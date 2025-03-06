b3453fd64b914dc180ae85ed97116da384c44f7d
PootisMate <162705848+PootisMate@users.noreply.github.com>
2025-03-06 08:05:00 -0300

    Add files via upload

---
+++ JogoDeDados.py
@@ -0,0 +1,8 @@
+import random
+
+Tipo_Dado = int(input('Qual tipo de dado você que usar?'))
+if (Tipo_Dado == 1);
+
+Quantidade = int(input('Quantos dados você quer jogar? '))
+Resposta = random.randint
+print(Resposta)
