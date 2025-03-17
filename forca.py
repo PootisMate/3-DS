25c32b675c680846abc8de7c4a9a15afad6996ee
PootisMate <162705848+PootisMate@users.noreply.github.com>
2025-03-17 08:56:27 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -1,15 +1,4 @@
 import random
-palavras = ["casa", "prato", "ceu"]
-
-sorteada = random.randint(0,2)
-
-print("A Palavra sorteada foi " + palavras[sorteada])
-print(len(palavras[sorteada]))
-
-letra = input("Digite a letra que você deseja buscar")
-
-i = 0
-while i < len(palavras[sorteada]):
-    if palavras[sorteada][i] == letra:
-        print("Palavra encontrada na posição " + str(i))
-    i += 1
+palavras = ["casa", "céu", "escola", "carro"]
+escolha = palavras[random.randint(0, len(palavras)-1)]
+print("_" * len(escolha))
